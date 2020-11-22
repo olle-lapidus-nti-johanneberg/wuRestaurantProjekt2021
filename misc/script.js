6f2730b2eb0cf7df62a7045e2257e4b6bf5eaa38
Olle <olle.lapidus@elev.ga.ntig.se>
2020-11-22 14:24:53 +0100

    bugfix
    fixed a bug where the newly added js hindered the header text from loading in

--- misc/script.js
+++ misc/script.js
@@ -3,18 +3,26 @@ let index = 0;
 let header_element = document.getElementById("headerText");
 let header_text = header_element.textContent;
 header_element.textContent = "";
+let possibleFilters = ["invert(58%) sepia(67%) saturate(3290%) hue-rotate(163deg) brightness(104%) contrast(108%)",
+        "invert(100%) sepia(38%) saturate(3260%) hue-rotate(34deg) brightness(122%) contrast(101%)",
+        "invert(32%) sepia(99%) saturate(1229%) hue-rotate(333deg) brightness(104%) contrast(108%)",
+        "invert(98%) sepia(4%) saturate(5161%) hue-rotate(78deg) brightness(103%) contrast(102%)",
+        "invert(22%) sepia(50%) saturate(7310%) hue-rotate(316deg) brightness(103%) contrast(101%)",
+        "invert(8%) sepia(69%) saturate(5917%) hue-rotate(317deg) brightness(99%) contrast(96%)"];
 
-function loadInHeader() {
-    console.log(index)
+function loadInPage() {
+    if (document.getElementById("gradientColorSauce") != null) {
+        document.getElementById("gradientColorSauce").style.filter = possibleFilters[Math.floor(Math.random() * possibleFilters.length)];
+    }
     if (index < header_text.length) {
         header_element.textContent += header_text.charAt(index);
         index++;
-        setTimeout(loadInHeader, 80);
     }
+    setTimeout(loadInPage, 80);
 }
 
 window.onload = function() {
-    loadInHeader();
+    loadInPage();
 }
 
 
@@ -28,20 +36,3 @@ window.onscroll = function() {
         navbar.style.position = "sticky";
     }
 };
-
-
-window.onload = function() {
-    let possibleFilters = ["invert(58%) sepia(67%) saturate(3290%) hue-rotate(163deg) brightness(104%) contrast(108%)",
-        "invert(100%) sepia(38%) saturate(3260%) hue-rotate(34deg) brightness(122%) contrast(101%)",
-        "invert(32%) sepia(99%) saturate(1229%) hue-rotate(333deg) brightness(104%) contrast(108%)",
-        "invert(98%) sepia(4%) saturate(5161%) hue-rotate(78deg) brightness(103%) contrast(102%)",
-        "invert(22%) sepia(50%) saturate(7310%) hue-rotate(316deg) brightness(103%) contrast(101%)",
-        "invert(8%) sepia(69%) saturate(5917%) hue-rotate(317deg) brightness(99%) contrast(96%)"];
-    
-    updateColor = function() {
-        document.getElementById("gradientColorSauce").style.filter = possibleFilters[Math.floor(Math.random() * possibleFilters.length)];
-        setTimeout(updateColor, 500)
-    }
-
-    setTimeout(updateColor, 500)
-}
