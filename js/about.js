4835ac07a181e57f28de94e02155d786bdd7e0bc
Olle <olle.lapidus@elev.ga.ntig.se>
2020-12-07 20:16:36 +0100

    about dev
    starting styling. added about.js.

---
+++ js/about.js
@@ -0,0 +1,15 @@
+
+function scrollWhoVision() {
+    window.scrollTo(window.scrollX, 
+        document.getElementById("our-vision").offsetTop - 70);
+}
+
+function scrollSocialMedia() {
+    window.scrollTo(window.scrollX, 
+        document.getElementById("our-social-media").offsetTop - 70);
+}
+
+function scrollReviews() {
+    window.scrollTo(window.scrollX, 
+        document.getElementById("our-reviews").offsetTop - 70);
+}
